b5cdfc740cca2a0f5ad70e8323c4d3eb7f912935
zhandong <1468767640@qq.com>
2019-06-01 23:33:33 +0800

    add zhanst

--- tree.go
+++ tree.go
@@ -2,15 +2,16 @@ package zin
 
 type methodTree struct {
 	method string
-	root   *node
+	root   *treeNode
 }
 
 type methodTrees map[string]methodTree
 
-type node struct {
+type treeNode struct {
 	path     string
-	child    []*node
+	child    []*treeNode
 	handlers HandlerChain
+	indices  string
 }
 
 func (tree methodTree) getValue(path string) (HandlerChain, Params) {
@@ -18,5 +19,26 @@ func (tree methodTree) getValue(path string) (HandlerChain, Params) {
 }
 
 func (tree methodTree) addRoute(path string, handlers HandlerChain) {
+	node := tree.root
+	if node == nil {
+		node := new(treeNode)
+		node.path = "/"
+		node.child = make([]*treeNode, 0)
+		node.handlers = make(HandlerChain, 0)
+		tree.root = node
+	}
+walk:
+	for {
+		if len(path) < len(node.path) {
+			var end int
+			for end = 0; end < len(path); end++ {
+				if path[end] != node.path[end] {
+					break
+				}
+			}
+			path = path[end:]
 
+		}
+		continue walk
+	}
 }
