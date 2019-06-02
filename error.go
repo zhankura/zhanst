c621441c0705f470f7f2716ad204e80a0f80f8cd
zhandong <1468767640@qq.com>
2019-06-02 20:09:18 +0800

    finish bind

--- error.go
+++ error.go
@@ -1,1 +1,13 @@
 package zhanst
+
+import "fmt"
+
+type Error struct {
+	Code     int
+	HttpCode int
+	Msg      string
+}
+
+func (err Error) Error() string {
+	return fmt.Sprintf("code: %v http code: %v msg: %v", err.Code, err.HttpCode, err.Msg)
+}
